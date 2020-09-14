61adbfd1eb28ba25a4b2f6ed8cb03e686f6e68d5
francois-tourigny <francois.tourigny@polymtl.ca>
2020-09-14 17:54:22 +0000

    Auto commit

--- exercice.py
+++ exercice.py
@@ -15,6 +15,7 @@ def to_radians(angle_degs: float, angle_mins: float, angle_secs: float) -> float
 
 def to_degrees(angle_rads: float) -> tuple:
     degs = 180*angle_rads/math.pi
+    
     return 0.0, 0.0, 0.0
 
 
