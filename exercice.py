92dfaee88e5dfaa85e61284d9e65265f8e1edbf0
francois-tourigny <francois.tourigny@polymtl.ca>
2020-09-14 17:52:17 +0000

    Auto commit

--- exercice.py
+++ exercice.py
@@ -9,7 +9,7 @@ def average(a: float, b: float, c: float) -> float:
     return (a+b+c)/3
 
 def to_radians(angle_degs: float, angle_mins: float, angle_secs: float) -> float:
-    degs = angle_degs + angle_mins/60 + 
+    degs = angle_degs + angle_mins/60 + angle_secs
     return 0.0
 
 
